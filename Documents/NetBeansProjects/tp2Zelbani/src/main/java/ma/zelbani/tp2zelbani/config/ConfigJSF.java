ec4de4bbc9e221d73fd0cd388a74917f4f6db744
ayoub zelbani <ayoub.zelbani2@gmail.com>
2023-10-31 15:42:59 +0100

    Exercice 1

---
+++ Documents/NetBeansProjects/tp2Zelbani/src/main/java/ma/zelbani/tp2zelbani/config/ConfigJSF.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ma.zelbani.tp2zelbani.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
